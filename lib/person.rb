df9956abe3b1e8bfd7561b0f773dad55fd872542
AjayGhimire <ajayghimire1998@gmail.com>
2022-05-11 21:44:09 +1000

    commit tree f433306306047f03f3f15242543e0deba40c093e

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
-# your code goes here
+class Person
+    attr_reader :name
+
+    def initialize name, bank_account
+        @name = name
+        @bank_account = bank_account
+    end
+
+    def
+end
+
+ajay = Person.new("Ajay", 25)
